864841562626ba138aa5ee5f2c3e1baef643296b
philingyuup <jialeyu@gmail.com>
2020-10-19 06:18:30 -0400

    Updates

---
+++ precache-manifest.6c66fc441de1237831b53644ca38ef46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc73188f007ddc0846e7ecd5dc4d3fb0",
+    "url": "/nayahackathon/index.html"
+  },
+  {
+    "revision": "0c6b35809c63cbc22751",
+    "url": "/nayahackathon/static/css/main.d41f939e.chunk.css"
+  },
+  {
+    "revision": "739d88ce99b55aa43bdc",
+    "url": "/nayahackathon/static/js/2.29654acc.chunk.js"
+  },
+  {
+    "revision": "367d0bc1f4ce0d0b4e75cfad70c99eb5",
+    "url": "/nayahackathon/static/js/2.29654acc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c6b35809c63cbc22751",
+    "url": "/nayahackathon/static/js/main.bb03c3e8.chunk.js"
+  },
+  {
+    "revision": "0d182e9cfc4a7f3a7dbd",
+    "url": "/nayahackathon/static/js/runtime-main.b799333a.js"
+  }
+]);
